cf2aca46ee60ac739c6b63465d982bd2d181a3ea
rafaelfbr <suportereipcp@gmail.com>
2026-04-07 07:28:31 -0300

    ajustado pra buscar na view

--- scripts/debug_maria_142.ts
+++ scripts/debug_maria_142.ts
@@ -9,7 +9,7 @@ async function checkMaria() {
   const mariaConnection = await mysql.createConnection(dbUrl);
 
   const [rows] = await mariaConnection.execute<mysql.RowDataPacket[]>(
-    "SELECT * FROM prensavulc_6 WHERE id > ? ORDER BY id ASC LIMIT 10",
+    "SELECT * FROM log_prensas WHERE view_id > ? ORDER BY view_id ASC LIMIT 10",
     [142]
   );
   
